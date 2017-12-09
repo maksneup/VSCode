9832d841ea32e12bc95fb8f9e4dc88587817e84e
maksneup <maksneup@yandex.ru>
2017-12-09 15:06:06 +1000

    init commit

---
+++ Create.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir /home/maksneup/Документы/Test3
+touch /home/maksneup/Документы/Test3/Lalsfsal.s
+touch /home/maksneup/Документы/Test3/fafafwfsaf
+touch /home/maksneup/Документы/Test3/asfwfsafw
+mkdir /home/maksneup/Документы/Test3/Test4
+touch /home/maksneup/Документы/Test3/Test4/fsafhjijhui
+rm -r /home/maksneup/Документы/Test1/
+mkdir /home/maksneup/Документы/Test1
